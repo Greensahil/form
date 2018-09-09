8dab4209c28422f561a1b58a366d58b52db1b546
unknown <ssharma2@svsu.edu>
2018-09-09 05:52:42 -0400

    add printed pages done

--- public/scripts.js
+++ public/scripts.js
@@ -460,10 +460,40 @@ var signaturePad3 = new SignaturePad(document.getElementById('signature-pad-3'),
     throttle: 16, // max x milli seconds on event update, OBS! this introduces lag for event update
     minPointDistance: 3,
 });
+var signaturePad4 = new SignaturePad(document.getElementById('signature-pad-4'), {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)',
+    velocityFilterWeight: .7,
+    minWidth: 0.5,
+    maxWidth: 2.5,
+    throttle: 16, // max x milli seconds on event update, OBS! this introduces lag for event update
+    minPointDistance: 3,
+});
+var signaturePad5 = new SignaturePad(document.getElementById('signature-pad-5'), {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)',
+    velocityFilterWeight: .7,
+    minWidth: 0.5,
+    maxWidth: 2.5,
+    throttle: 16, // max x milli seconds on event update, OBS! this introduces lag for event update
+    minPointDistance: 3,
+});
+var signaturePad6 = new SignaturePad(document.getElementById('signature-pad-6'), {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)',
+    velocityFilterWeight: .7,
+    minWidth: 0.5,
+    maxWidth: 2.5,
+    throttle: 16, // max x milli seconds on event update, OBS! this introduces lag for event update
+    minPointDistance: 3,
+});
 var saveButton = document.getElementById('save'),
     clearButton = document.getElementById('clear'),
     clearButton2 = document.getElementById('clear-2')
     clearButton3 = document.getElementById('clear-3')
+    clearButton4 = document.getElementById('clear-4')
+    clearButton5 = document.getElementById('clear-5')
+    clearButton6 = document.getElementById('clear-6')
     showPointsToggle = document.getElementById('showPointsToggle');
 
 clearButton.addEventListener('click', function(event) {
@@ -474,4 +504,13 @@ clearButton2.addEventListener('click', function(event) {
 });
 clearButton3.addEventListener('click', function(event) {
     signaturePad3.clear();
+});
+clearButton4.addEventListener('click', function(event) {
+    signaturePad4.clear();
+});
+clearButton5.addEventListener('click', function(event) {
+    signaturePad5.clear();
+});
+clearButton6.addEventListener('click', function(event) {
+    signaturePad6.clear();
 });
