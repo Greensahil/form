d8397f668be817fd71a7df7e5cfb9ad09227de58
unknown <ssharma2@svsu.edu>
2018-09-10 07:32:20 -0400

    ds

--- index.js
+++ index.js
@@ -124,63 +124,7 @@ app.post('/setup', function(req, res, next) {
     
 
 });
-var encodedpdf
-app.get('/apple',function(req,res){
-    const output=`
-    <p>You have a new contact request</p>
-    <h3>Contact detalis</h3>
-    <ul>
-    <li>Name:${req.body.name}</li>
-    <li>Email:${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-    `
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.mailgun.org',
-        port: 2525,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: keys.user, // generated ethereal user
-            pass: keys.pass // generated ethereal password
-        },
-        tls:{
-            rejectUnauthorized:false
-        }
-    });
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Nodemailer contact" <foo@example.com>', // sender address
-        to: 'greenncoder@gmail.com', // list of receivers
-        subject: 'Node contact request', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output, // html body
-        attachments: [{
-            filename: 'transportpdf.pdf',
-            content: encodedpdf,
-            encoding: 'base64'
-          }]
 
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-    // req.flash("success","Message has been sent")
-
-    // res.render('index',{success:req.flash("success")})
-    
-    
-})
 app.listen(process.env.PORT||3000, function(){
     console.log("The Server Has Started!");
  });
