1198ba844893af8215d0580714b48a62104e69f8
unknown <ssharma2@svsu.edu>
2018-09-13 04:41:59 -0400

    sdsd

--- public/loadingpage.js
+++ public/loadingpage.js
@@ -1,25 +1,23 @@
-  function pageloader(){
-    var counter = 0;
-    var c = 0;
-    var i = setInterval(function(){
-        $(".loading-page .counter h1").html(c + "%");
-        $(".loading-page .counter hr").css("width", c + "%");
-        //$(".loading-page .counter").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
-  
-      /*
-      $(".loading-page .counter h1.color").css("width", c + "%");
-      */
-      counter++;
-      c++;
-        
-      if(counter == 101) {
-          clearInterval(i);
-          
-          console.log('now')
-          toggleDiv('loader')
-      }
-    }, 25);
+function pageloader(){
+  var counter = 0;
+  var c = 0;
+  var i = setInterval(function(){
+      $(".loading-page .counter h1").html(c + "%");
+      $(".loading-page .counter hr").css("width", c + "%");
+      //$(".loading-page .counter").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
 
-    $('meta[name="viewport"]').prop('content', 'width=device-width, initial-scale=1, shrink-to-fit=no');
-  }
-    
+    /*
+    $(".loading-page .counter h1.color").css("width", c + "%");
+    */
+    counter++;
+    c++;
+      
+    if(counter == 101) {
+        clearInterval(i);
+        $('meta[name="viewport"]').prop('content', 'width=device-width, initial-scale=1, shrink-to-fit=no');
+      //   toggleDiv('loader')
+    }
+  }, 25);
+
+  
+}
